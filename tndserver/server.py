efe6adecc43ec283c86bddfddb221691ebe9eb41
Administrator <admin@local.host>
2016-07-17 18:52:57 +0900

    waiting room implement done

--- tndserver/server.py
+++ tndserver/server.py
@@ -1,6 +1,7 @@
 import tornado.ioloop
 import tornado.web
 import tornado.websocket
+import json
 
 from tornado.options import define, options, parse_command_line
 
@@ -8,7 +9,8 @@ define("port", default=8888, help="run on the given port", type=int)
 
 # we gonna store clients in list..
 clients = list()
-# 
+#
+
 class IndexHandler(tornado.web.RequestHandler):
     @tornado.web.asynchronous
     def get(self):
@@ -21,20 +23,46 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler):
     def open(self, *args):
         print "WebSocketHandler.open"
         self.stream.set_nodelay(True)
-        clients.append(self)
 
     def on_message(self, message):
-        """
-        when we receive some message we want some message handler..
-        for this example i will just print message to console
-        """
-        for client in clients :
-            client.write_message(message)
+        js = json.loads(message)
+        if js['msg'] == 'connect' :
+            self.id = js['id']
+            clients.append(self)
+
+            # send adduser msg to existed clients
+            dictAddUser = {'msg':'adduser'}
+            dictAddUser['id'] = js['id']
+            msgAddUser = json.dumps(dictAddUser)
+            for client in clients :
+                if client != self :
+                    client.write_message(msgAddUser)
+
+            # send alluser msg to new client
+            dictAllUser = {'msg':'alluser'}
+            dictAllUser['num'] = len(clients)
+
+            i = 0
+            dictUserList = dict()
+            for client in clients :
+                dictUserList[i] = client.id
+                i += 1
+            dictAllUser['users'] = dictUserList
+
+            msgAllUser = json.dumps(dictAllUser)
+            self.write_message(msgAllUser)
 
     def on_close(self):
         print "WebSocketHandler.on_close"
         if self in clients:
             clients.remove(self)
+
+            # send deluser msg to existed clients
+            dictDelUser = {'msg': 'deluser'}
+            dictDelUser['id'] = self.id
+            msgDelUser = json.dumps(dictDelUser)
+            for client in clients:
+                client.write_message(msgDelUser)
 
 app = tornado.web.Application([
     (r'/', IndexHandler),
